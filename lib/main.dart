846f66ff16107863c061fba741c228fc6ea36597
boris6 <sijanb@outlook.com>
2023-05-29 21:34:24 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:expensetracker/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
